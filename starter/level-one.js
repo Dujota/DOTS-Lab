b431e63ae2cf72c5e6338db2575a59fed207138d
kelliehogue42 <136033105+kelliehogue42@users.noreply.github.com>
2023-06-25 10:27:14 -0700

    Add files via upload

--- starter/level-one.js
+++ starter/level-one.js
@@ -1,4 +1,25 @@
-/*
- * DOTS: Level One
- *
- */
+// level-one.js
+
+// Create necessary variables
+let score = 0;
+const ball = document.querySelector('.js-ball');
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+// Add event listener to ball element
+ball.addEventListener('click', function() {
+  // Increment the score by 10
+  score += 10;
+
+  // Update the score display
+  scoreDisplay.innerText = score;
+
+  // Check if the player's score reaches 100
+  if (score >= 100) {
+    // Show the win message by setting opacity to 1
+    levelWinner.style.opacity = '1';
+  }
+
+  // Console log for testing purposes
+  console.log('Score:', score);
+});
--- starter/level-three.js
+++ starter/level-three.js
@@ -1,4 +1,29 @@
-/*
- * DOTS: Level Three
- *
- */
+// level-three.js
+
+// Create necessary variables
+let score = 0;
+const balls = document.querySelectorAll('.js-ball');
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+// Add event listener to each ball element
+balls.forEach(ball => {
+  const increment = parseInt(ball.getAttribute('data-increment'));
+
+  ball.addEventListener('click', function() {
+    // Increment the score by the specified value
+    score += increment;
+
+    // Update the score display
+    scoreDisplay.innerText = score;
+
+    // Check if the player's score reaches 100
+    if (score >= 100) {
+      // Show the win message by setting opacity to 1
+      levelWinner.style.opacity = '1';
+    }
+
+    // Console log for testing purposes
+    console.log('Score:', score);
+  });
+});
