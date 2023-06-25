3d5ed5a1cf39ef03441100e241779e6a71f510eb
Karla Ortiz <karla.ortiz@codenation.org>
2023-06-25 12:19:02 -0700

    level 1 js completed

--- starter/level-one.js
+++ starter/level-one.js
@@ -4,26 +4,22 @@
 
 let score = 0;
 const ball = document.querySelector(".js-ball");
-const scoreDisplay = document.querySelector(".js-score");
+let scoreDisplay = document.querySelector(".js-score");
 const levelWinner = document.querySelector(".level-winner");
 
 //Callback Functions
-
-/** LEVEL 1 - 
- *  when a player interacts with Level 1, their `score` display should update as they click the `ball` and they should have a win message appear when their `score` reaches `100`
-         * 1- we should increment the `score` by `10` when the ball is clicked
-                * if ball is clicked, then store/take the value of the content and add 10 to it, keep doing that through a loop - everytime it is cliked
-         * 2- make sure that a player sees that their score has gone up
-                 * store the value of the score and use innerText or innerHTML to add the score to HTML - Set the `innerText` property of `scoreDisplay` equal to `score` to accomplish this 
-         * 3- Write a conditional statement that checks if the player's `score` is `greater than or equal to 100` - winning score
-             * If their `score` is at or above `100` we'll need to reflect that by showing the player that they've beaten the level
-             * To show that the player has won, we'll need to target `opacity` within the `style` property of `levelWinner` and set its value to `1`
-     */
-
-const increaseScoreByTen = () =>{
-        
+const increaseScoreByTen = () =>{ 
+        score = score+10;
+        let scoreString = score.toString();
+        scoreDisplay.innerText=scoreString.padStart(4, "0");
+}
+const winLevelOne = ()=>{
+        if (score>=100){
+                levelWinner.style.opacity = 1;
+        }
 }
 //Event Listeners
 ball.addEventListener('click', function(){
-
+        increaseScoreByTen();
+        winLevelOne();
 });
