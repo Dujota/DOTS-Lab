f9d83ec672711d7e15126b5f136bb3c286d1012a
Karla Ortiz <karla.ortiz@codenation.org>
2023-06-25 22:41:45 -0700

    level three js complete

--- starter/level-three.js
+++ starter/level-three.js
@@ -1,4 +1,41 @@
-/*
- * DOTS: Level Three
- *
- */
+//Variables
+let score = 0;
+const ball = document.querySelectorAll(".js-ball");
+const scoreDisplay = document.querySelector(".js-score");
+const levelWinner = document.querySelector(".level-winner");
+
+const levelThreeWinner = () =>{
+    if(score>=100){
+    levelWinner.style.opacity=1;
+}
+}
+
+
+ball[0].addEventListener('click', event=>{
+    const scoreValue = Number(ball[0].dataset.increment);
+    score = score +scoreValue;
+    const scoreString = score.toString();
+    scoreDisplay.innerText = scoreString.padStart(4, "0");
+    levelThreeWinner()
+})
+
+
+ball[1].addEventListener('click', event=>{
+    const scoreValue = Number(ball[1].dataset.increment);
+    score = score +scoreValue;
+    const scoreString = score.toString();
+    scoreDisplay.innerText = scoreString.padStart(4, "0");
+    levelThreeWinner()
+})
+
+
+ball[2].addEventListener('click', event=>{
+    const scoreValue = Number(ball[2].dataset.increment);
+    score = score +scoreValue;
+    const scoreString = score.toString();
+    scoreDisplay.innerText = scoreString.padStart(4, "0");
+    levelThreeWinner()
+})
+
+
+
