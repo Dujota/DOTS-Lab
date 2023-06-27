737976f1744f89d94f223b8bf99b8caffe0ab98a
carolis546 <carolina.salinas@codenation.org>
2023-06-27 16:52:57 -0500

    level2

--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,19 @@
  * DOTS: Level Two
  *
  */
+
+let score=0;
+let ball=document.querySelector(".js-ball");
+let scoreDisplay=document.querySelector(".js-score");
+let levelWinner=document.querySelector(".level-winner")
+
+const elements = document.querySelectorAll('.js-ball');
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function() {
+        score+=10;
+        document.querySelector(".js-score").innerHTML = score;
+    if (score>="100") {
+        levelWinner.style.opacity ="1";
+    }
+    });
+  };
