06662fd66aaaff1ca841dceb842f5210a85ccbc6
Rianna Winston <riannawinston@macbook-air.myfiosgateway.com>
2023-06-26 20:53:20 -0400

    first commit

--- starter/level-one.js
+++ starter/level-one.js
@@ -1,4 +1,13 @@
-/*
- * DOTS: Level One
- *
- */
+score = document.querySelector('.js-score').textContent
+ball = document.querySelector('.js-ball')
+
+
+
+addScore = () => {
+    let score = 0
+    score += 10
+    console.log(score)
+    score.innerText = 'score'
+}
+
+ball.addEventListener('click', addScore)
