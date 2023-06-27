2b6f442eed98ad79513794e45b5ebc2957dab64f
Rianna Winston <riannawinston@macbook-air.myfiosgateway.com>
2023-06-26 21:59:01 -0400

    level one complete

--- starter/level-one.js
+++ starter/level-one.js
@@ -3,17 +3,16 @@ const ball = document.querySelector('.js-ball')
 const levelWinner = document.querySelector('.level-winner')
 
 
-
-
 ball.addEventListener('click', function() {
-    let score = scoreDisplay.innerHTML
-    if (score < 100){
-        score += 10
-        console.log(score)
-        scoreDisplay.innerHTML = score
-    } else {
-        let Winner = document.querySelector('.level-winner')
+   
+   let score = Number(scoreDisplay.innerHTML)+ 10;
+    scoreDisplay.innerHTML = score
+
+
+    if (score >= 100) {
+        Winner = document.querySelector('.level-winner')
         Winner.style.opacity = '1';
         scoreDisplay.innerHTML = `100`
     }
+
 });
