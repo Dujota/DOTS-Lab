f08b00aac6f7cc7b317634bf0832d58f8e2d0451
carolis546 <carolina.salinas@codenation.org>
2023-06-27 16:39:45 -0500

    level1

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,18 @@
  * DOTS: Level One
  *
  */
+
+let score=0;
+let ball=document.querySelector(".js-ball");
+let scoreDisplay=document.querySelector(".js-score");
+let levelWinner=document.querySelector(".level-winner")
+
+
+ball.addEventListener('click', function() {
+    score+=10;
+    document.querySelector(".js-score").innerHTML = score;
+if (score>="100") {
+    levelWinner.style.opacity ="1";
+}
+})
+     
