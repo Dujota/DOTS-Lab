923f8d1640c13c65f454b5aefc5f2dcdceb41ead
MirafuentesD <denilsonmirafuentes3@gmail.com>
2023-06-27 14:56:31 -0700

    Level one code completed

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,26 @@
  * DOTS: Level One
  *
  */
+var score = 0;
+const ball = document.querySelector(".js-ball");
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+const ballClicked = (Event) => {
+    // some logic for updating score and checking for a win here
+    // update score by 10
+    console.log("ball clicked");
+    score += 10;
+
+    scoreDisplay.innerText = score;
+
+    if (score >= 100){
+        levelWinner.style.opacity = 1;
+    };
+
+
+  };
+
+ball.addEventListener('click', ballClicked);
+
+
