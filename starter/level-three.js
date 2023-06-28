eb7b47926fe8b189d69590242a5321f73666b7b4
Rianna Winston <riannawinston@macbook-air.myfiosgateway.com>
2023-06-27 21:06:15 -0400

    level three complete

--- starter/level-three.js
+++ starter/level-three.js
@@ -1,4 +1,23 @@
-/*
- * DOTS: Level Three
- *
- */
+let scoreDisplay = document.querySelector('.js-score')
+const ball = document.querySelectorAll('.js-ball')
+const levelWinner = document.querySelector('.level-winner')
+
+
+for (let i = 0; i < ball.length; i++) {
+    ball[i].addEventListener('click', function() {
+
+        let increment = parseInt(this.dataset.increment);
+
+        let score = Number(scoreDisplay.innerHTML)+ increment;
+            scoreDisplay.innerHTML = score
+        
+      
+
+        if (score >= 100) {
+            Winner = document.querySelector('.level-winner')
+            Winner.style.opacity = '1';
+            scoreDisplay.innerHTML = `100`
+        }
+  });
+};
+   
