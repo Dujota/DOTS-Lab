ccf85c7fb6620fe4f62cf33d441c5a13c9b81ca6
Frank <51930059+fxl4@users.noreply.github.com>
2023-07-04 18:22:25 -0400

    Add files via upload

--- starter/level-one.js
+++ starter/level-one.js
@@ -1,4 +1,29 @@
-/*
- * DOTS: Level One
- *
- */
+let scoreDisplay = 0;
+
+function levelWinner() {
+    document.querySelector(".level-winner").style.opacity = 1;
+    document.body.addEventListener('click', e => {
+        window.location.href = "/level-two.html"; 
+    }
+)};
+
+// Element Selectors
+const gameBall = document.querySelector(".js-ball");
+
+// Event Handlers
+function clickedBall(event) {
+    if (scoreDisplay < 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+    } else if (scoreDisplay === 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+        levelWinner();
+    } 
+};
+
+// Event Listeners
+gameBall.addEventListener('click', e => {
+	clickedBall();
+	e.stopPropagation();
+});
--- starter/level-three.js
+++ starter/level-three.js
@@ -1,4 +1,66 @@
-/*
- * DOTS: Level Three
- *
- */
+let scoreDisplay = 0;
+function levelWinner() {
+    document.querySelector(".level-winner").style.opacity = 1;
+    document.querySelector("#gameBall1").style.opacity = 0;
+    document.querySelector("#gameBall2").style.opacity = 0;
+    document.querySelector("#gameBall3").style.opacity = 0;
+    document.body.addEventListener('click', e => {
+        document.querySelector(".level-winner").style.opacity = 0;
+        document.querySelector(".game-over").style.opacity = 1;
+        }
+)};
+
+// Element Selectors
+document.querySelector("#gameBall1");
+document.querySelector("#gameBall2");
+document.querySelector("#gameBall3");
+
+// Event Handlers
+function clickedBall1(event) {
+    if (scoreDisplay < 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+    } else if (scoreDisplay === 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+        levelWinner();
+    } 
+};
+
+function clickedBall2(event) {
+    if (scoreDisplay < 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+    } else if (scoreDisplay === 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+        levelWinner();
+    } 
+};
+
+function clickedBall3(event) {
+    if (scoreDisplay < 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+    } else if (scoreDisplay === 90) {
+        scoreDisplay += 10;
+        document.querySelector(".js-score").innerHTML = scoreDisplay;
+        levelWinner();
+    } 
+};
+
+// Event Listeners
+gameBall1.addEventListener('click', e => {
+	clickedBall1();
+	e.stopPropagation();
+});
+
+gameBall2.addEventListener('click', e => {
+	clickedBall2();
+	e.stopPropagation();
+});
+
+gameBall3.addEventListener('click', e => {
+	clickedBall3();
+	e.stopPropagation();
+});
