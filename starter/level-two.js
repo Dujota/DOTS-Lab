fbb3f6985e4f6d38a287017f6a924f5668118239
Pozzi <naomipozzi@gmail.com>
2023-07-11 21:26:48 -0400

    Level 2 updates

--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,26 @@
  * DOTS: Level Two
  *
  */
+const elements = document.querySelectorAll('.js-ball')
+//const scoreDisplay = document.querySelector('.score')
+const scoreDisplay = document.querySelector('.js-score')
+//If starting Level 2, then the score should be equal to 100 which is the Level 1 threshold
+let score  = 0
+
+
+for (let i = 0;i < elements.length; i++) {
+    elements[i].addEventListener('click', function() {
+        score = score += 10
+        scoreDisplay.innertext = score
+        console.log("Score = ", score)
+        console.log("scoreDisplay =", scoreDisplay.innertext)
+        console.log("Elements = ", elements)
+   
+    //change to 100
+        if (score >= 30) {
+           // levelWinner.style.opacity = 1;
+            console.log("score 300 or greater and opacity =", levelWinner.style.opacity)
+        } 
+    });
+  
+    }
