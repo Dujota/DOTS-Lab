4e244affdf42f0074d79d807c708ecbf84554c17
Asti <astistateofmind@gmail.com>
2023-10-18 15:54:52 -0700

    level 3

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,27 @@
  * DOTS: Level Three
  *
  */
+
+const scoreDisplay = document.querySelector('.js-score')
+const balls = document.querySelectorAll('.js-ball')
+const levelWinner = document.querySelector('.level-winner')
+
+let clickCount = 0
+
+function trackClicks(e) {
+    const clickedBall = e.currentTarget;
+    const amountOfPoints = parseInt(clickedBall.dataset.increment);
+    clickCount = clickCount + amountOfPoints;
+    scoreDisplay.innerText = clickCount;
+
+
+    if (clickCount >= 100) {
+        levelWinner.style.opacity = 1;
+    } else {
+        levelWinner.style.opacity = 0;
+    }
+}
+
+balls.forEach(ball => {
+    ball.addEventListener('click', trackClicks);
+});
