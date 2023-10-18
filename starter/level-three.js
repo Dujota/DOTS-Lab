7426d60ee6423f3247ef91997248ac1966efa860
Daniel Richardson <134252158+aphiddd@users.noreply.github.com>
2023-10-18 19:55:23 -0400

    Completed scripting for game
    HTML files are here too because I made a very small change to them.

--- starter/level-three.js
+++ starter/level-three.js
@@ -1,4 +1,16 @@
-/*
- * DOTS: Level Three
- *
- */
+const ball = document.querySelectorAll('.js-ball');
+const counterEl = document.querySelector('.js-score');
+const winnerEl = document.querySelector('.level-winner');
+
+var score = 0;
+
+ball.forEach(el => el.addEventListener('click', function () {
+    if (score < 100) {
+        const ballScore = Number(el.getAttribute("data-increment"))
+        score = score + ballScore;
+        counterEl.innerText = score.toString();
+        if (score >= 100){
+            counterEl.innerText = 100
+            winnerEl.style.setProperty('opacity', 100);
+        }
+}}))
