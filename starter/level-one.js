f2336eebf550c57440ad4aa6f1a2d0206c7d2089
Asti <astistateofmind@gmail.com>
2023-10-18 15:40:02 -0700

    level 1

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,22 @@
  * DOTS: Level One
  *
  */
+
+const scoreDisplay = document.querySelector('.js-score')
+const ball = document.querySelector('.js-ball')
+const levelWinner = document.querySelector('.level-winner')
+
+let clickCount = 0
+
+function trackClicks() {
+    clickCount = clickCount + 10
+    scoreDisplay.innerText = clickCount
+
+    if (clickCount === 100) {
+        levelWinner.style.opacity = 1;
+    } else {
+        levelWinner.style.opacity = 0;
+    }
+}
+
+ball.addEventListener('click', trackClicks)
