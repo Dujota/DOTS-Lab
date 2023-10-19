61ace302f64ce145939f981775a358d03ce3d1f5
El Zee <53586135+itzlawyo@users.noreply.github.com>
2023-10-18 20:31:18 -0400

    commit 3 JS files

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,26 @@
  * DOTS: Level One
  *
  */
+
+let score = 0;
+
+const ball = document.querySelector('.js-ball');
+let scoreDisplay = document.querySelector('.score.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+
+ball.addEventListener('click', function() {
+    score += 10;
+    scoreDisplay.innerText = padNumber(score);
+    checkScore(score);
+});
+
+function checkScore(score) {
+    if(score >= 100) {
+        levelWinner.style.opacity = 1;
+    }
+}
+
+function padNumber(number) {
+    return number.toString().padStart(4, '0');
+}
