f151a9cfa6e86cf3e50ecac50fec653b7fcb051c
david-n94 <david.nazaryann@gmail.com>
2023-10-25 16:24:04 -0400

    Add completed js files for levels 1-3

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,45 @@
  * DOTS: Level Three
  *
  */
+
+
+let ballElements = document.querySelectorAll(".js-ball")
+let scoreDisplay = document.querySelector(".js-score")
+let arena = document.querySelector(".js-arena")
+
+
+let levelWinner = document.querySelector(".level-winner")
+let score = 0
+
+
+for (i = 0; i < ballElements.length; i++) {
+    ballElements[i].addEventListener("click", function (event) {
+        const increment = parseInt(event.target.dataset.increment)
+        if (increment === 20) {
+            score += 20
+            scoreDisplay.innerText = score
+        } else if (increment === 10) {
+            score += 10
+            scoreDisplay.innerText = score
+        } else if (increment === 5) {
+            score += 5
+            scoreDisplay.innerText = score
+        }
+
+        if (score >= 100) {
+            levelWinner.style.opacity = 1;
+            arena.style.display = "none";
+
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,23 @@
  * DOTS: Level Two
  *
  */
+
+const ballElements = document.querySelectorAll(".js-ball")
+const arena = document.querySelector(".js-arena")
+
+let scoreDisplay = document.querySelector(".js-score")
+let levelWinner = document.querySelector(".level-winner")
+let score = 0
+
+
+
+for (let i = 0; i < ballElements.length; i++) {
+    ballElements[i].addEventListener('click', function () {
+        score += 10
+        scoreDisplay.innerText = score
+        if (score >= 100) {
+            levelWinner.style.opacity = 1;
+            arena.style.display = "none";
+        }
+    });
+};
