1241cd1a4579867c238b3fa7985ea226fc073daa
michellechoi126 <michellechoi126@gmail.com>
2023-11-01 09:01:41 -0700

    add level-one logic

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,19 @@
  * DOTS: Level One
  *
  */
+let score = 0
+
+//HTML Elements
+const ball = document.querySelector('.js-ball');
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+ball.addEventListener('click', function() {
+    score += 10
+    scoreDisplay.innerText = score
+    if (score >= 100) {
+        levelWinner.style.opacity = 1;
+    } else {
+        return
+    }
+});
