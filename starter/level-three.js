1f2a521abdf0b81f37a9bc81bdf2092d87ec00fc
michellechoi126 <michellechoi126@gmail.com>
2023-11-01 09:31:53 -0700

    add level-three logic

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,22 @@
  * DOTS: Level Three
  *
  */
+let score = 0;
+
+const balls = document.querySelectorAll('.js-ball');
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+for (let i=0; i<balls.length; i++) {
+    balls[i].addEventListener('click', function () {
+        increment = parseInt(balls[i].dataset.increment)
+        console.log(increment)
+        score += increment
+        scoreDisplay.innerText = score
+        if (score >= 100) {
+            levelWinner.style.opacity = 1
+        } else {
+            return
+        }
+    })
+}
